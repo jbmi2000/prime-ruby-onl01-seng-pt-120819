b48b3ff11710eafdf3a9e2b1a0bd0748bdb73c8d
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-19 19:19:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 
 primeArray = []
 def prime?(int)
-   if int.is < 0
+  if int < 0
+    break    
+  end 
+  if int == 2
    (1..100).each do |num|
-    if (1..num).select {|d| num % d == 0 && !(num < 0) } == [1,num]
+   elsif (1..num).select {|d| num % d == 0 && !(num < 0) } == [1,num]
       primeArray.push(num)
 
 end
