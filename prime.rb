292f717df7de4b01e163747bf8fc69da4b570661
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-19 19:34:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 
 primeArray = []
 def prime?(int)
-  int.all? do |num| |d| num % d == 0 && !(num < 0) == [1,num]
+  int.all? do |num| num % d == 0 && !(num < 0) == [1,num]
       primeArray.push(num)
 end
 end
