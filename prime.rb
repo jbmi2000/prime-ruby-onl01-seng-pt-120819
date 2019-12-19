2187c745bbddfc588a14163449e2917dddcec6f2
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-19 20:35:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(int)
   return false if !int.integer?
   return false if int < 2
   return true if int == 2
-if (2..int-1).each {|num| puts false if int % num == 0}
+if (2..int-1).each {|num| return false if int % num == 0}
   primeArray.push(int)
 end
 end
